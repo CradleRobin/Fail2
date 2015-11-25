2c17a5fbf7f80e5688da56c92e1aa0734f3685a4
CradleRobin <revansminion@gmail.com>
2015-11-25 15:50:22 -0600

    Full App Function
    0.001

---
+++ app/src/main/java/com/example/mrenaud/fail2/AppDetail.java
@@ -0,0 +1,11 @@
+package com.example.mrenaud.fail2;
+
+ import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+        CharSequence label;
+        CharSequence name;
+        Drawable icon;
+
+  }
+
--- app/src/main/java/com/example/mrenaud/fail2/HomeActivity.java
+++ app/src/main/java/com/example/mrenaud/fail2/HomeActivity.java
@@ -1,7 +1,6 @@
 package com.example.mrenaud.fail2;
 
 import android.content.Intent;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +9,13 @@ import android.view.View;
 public class HomeActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
     public void showApps(View v) {
-        Intent i = new Intent(this, Applistactivity.class);
+        Intent i = new Intent(this, AppListActivity.class);
         startActivity(i);
-        }
     }
+}
